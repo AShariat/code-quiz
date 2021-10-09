c99bde1b447cc6cdb3daca7c098b88acdd34cf97
Amir Shariat <amirshariat1987@gmail.com>
2021-10-08 20:16:24 -0700

    added function for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,61 @@
-var question1 = {
-  question: "1-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B1",
-  answer2: "C1",
-  answer3: "D1",
-  answer4: "E1"
-};
-var question2 = {
-  question: "2-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B2",
-  answer2: "C2",
-  answer3: "D2",
-  answer4: "E2"
-};
-var question3 = {
-  question: "3-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B3",
-  answer2: "C3",
-  answer3: "D3",
-  answer4: "E3"
-};
-var question4 = {
-  question: "4-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B4",
-  answer2: "C4",
-  answer3: "D4",
-  answer4: "E4"
-};
-var question5 = {
-  question: "5-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B5",
-  answer2: "C5",
-  answer3: "D5",
-  answer4: "E5"
-};
-var question6 = {
-  question: "6-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.",
-  answer1: "B6",
-  answer2: "C6",
-  answer3: "D6",
-  answer4: "E6"
-};
-var questions = [question1, question2, question3, question4, question5, question6];
+const dataBase = [
+  {question: "1-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B1", answer2: "C1", answer3: "D1", answer4: "E1"},
+  {question: "2-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B2", answer2: "C2", answer3: "D2", answer4: "E2"},
+  {question: "3-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B3", answer2: "C3", answer3: "D3", answer4: "E3"},
+  {question: "4-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B4", answer2: "C4", answer3: "D4", answer4: "E4"},
+  {question: "5-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B5", answer2: "C5", answer3: "D5", answer4: "E5"},
+  {question: "6-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, cumque, eius recusandae harum rem repudiandae quis fuga neque, eum autem explicabo illum. Accusantium voluptatem natus id architecto veniam dolor minima.", answer1: "B6", answer2: "C6", answer3: "D6", answer4: "E6"}];
 var start = document.getElementById('start');
-var question = document.getElementById('questions');
+var questions = document.getElementById('questions');
 var quiz = function() {
-  var i = 0;
-  selectedQuestion = function() {
-    console.log("Question " + i);
+  for (var i = 0; i < dataBase.length; i++) {
+    var newDivEl = document.createElement('div');
+    newDivEl.id = ('question');
+    newDivEl.className = ('questions');
+    questions.appendChild(newDivEl);
+    var questionEl = document.createElement('p');
+    questionEl.textContent = (dataBase[i].question);
+    newDivEl.appendChild(questionEl);
+    var answer1El = document.createElement('button');
+    answer1El.textContent = (dataBase[i].answer1);
+    answer1El.id = ('answer1');
+    answer1El.className = 'answers';
+    newDivEl.appendChild(answer1El);
+    var answer2El = document.createElement('button');
+    answer2El.textContent = (dataBase[i].answer2);
+    answer2El.id = ('answer2');
+    answer2El.className = 'answers';
+    newDivEl.appendChild(answer2El);
+    var answer3El = document.createElement('button');
+    answer3El.textContent = (dataBase[i].answer3);
+    answer3El.id = ('answer3');
+    answer3El.className = 'answers';
+    newDivEl.appendChild(answer3El);
+    var answer4El = document.createElement('button');
+    answer4El.textContent = (dataBase[i].answer4);
+    answer4El.id = ('answer4');
+    answer4El.className = 'answers';
+    newDivEl.appendChild(answer4El);
   }
-  select = function() {
-    for (i; i < questions.length; i) {
-      var newDivEl = document.createElement('div');
-      newDivEl.id = ('question' + i);
-      newDivEl.className = ('questions');
-      question.appendChild(newDivEl);
-      var questionEl = document.createElement('p');
-      questionEl.textContent = (questions[i].question);
-      newDivEl.appendChild(questionEl);
-      var answer1El = document.createElement('button');
-      answer1El.textContent = (questions[i].answer1);
-      newDivEl.appendChild(answer1El);
-      var answer2El = document.createElement('button');
-      answer2El.textContent = (questions[i].answer2);
-      newDivEl.appendChild(answer2El);
-      var answer3El = document.createElement('button');
-      answer3El.textContent = (questions[i].answer3);
-      newDivEl.appendChild(answer3El);
-      var answer4El = document.createElement('button');
-      answer4El.textContent = (questions[i].answer4);
-      newDivEl.appendChild(answer4El);
-      i++;
-      selectedQuestion();
-    }
-  }
-  select();
 };
+function stopTimer() {
+  clearInterval(intervalID);
+  document.querySelector('#questions').innerHTML = '<span>Time is up!</span>';
+};
+function startTimer() {
+  const timeDisplay = document.getElementById('time');
+  let timeRemaining = 60;
+  intervalID = setInterval(function () {
+    timeRemaining--;
+    timeDisplay.innerText = "Time Remaining: " + timeRemaining + " s";
+    if (timeRemaining <= 0) {
+      stopTimer();
+    }
+  }, 1000);
+}
 var startBtn = document.querySelector('#start-btn');
 startBtn.addEventListener('click', function(){
-  quiz();
   start.style.display = "none";
-  // .style.display = "block";
+  startTimer();
+  quiz();
 });
