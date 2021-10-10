7585641f13a443a635d7cc4fa31d6a878aa7b3b2
Amir Shariat <amirshariat1987@gmail.com>
2021-10-10 17:02:11 -0600

    added feedback function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,35 @@
 const dataBase = [
-  {question: 'Inside which HTML element do we put the JavaScript?', answer: [{text: '<scripting>', value:false}, {text:'<javascript>', value: false}, {text: '<js>', value: false}, {text: '<script>', value: true}]}, {question: 'How do you write "Hello World" in an alert box?', answer: [{text: 'msgBox("Hello World")', value:false}, {text:'msg("Hello World")', value: false}, {text: 'alertBox("Hello World")', value: false}, {text: 'alert("Hello World")', value: true}]}, {question: 'How do you create a function in JavaScript?', answer: [{text: 'myFunction() = function', value:false}, {text:'function myFunction()', value: true}, {text: 'function:myFunction()', value: false}, {text: 'function = myFunction()', value: false}]}, {question: 'How do you call a function named "myFunction"?', answer: [{text: 'myFunction', value:false}, {text:'call function myFunction()', value: false}, {text: 'call myFunction()', value: false}, {text: 'myFunction()', value: true}]}, {question: 'How to write an IF statement in JavaScript?', answer: [{text: 'if i == 5 then', value:false}, {text:'if i = 5', value: false}, {text: 'if i = 5 then', value: false}, {text: 'if (i == 5)', value: true}]}, {question: 'How does a FOR loop start?', answer: [{text: 'for (i = 0; i <= 5)', value:false}, {text:'for (i <= 5; i++)', value: false}, {text: 'for i = 1 to 5', value: false}, {text: 'for (i = 0; i <= 5; i++)', value: true}]}, {question: 'What is the correct way to write a JavaScript array?', answer: [{text: 'var colors = "red", "green", "blue"', value:false}, {text:'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', value: false}, {text: 'var colors = (1:"red", 2:"green", 3:"blue")', value: false}, {text: 'var colors = ["red", "green", "blue"]', value: true}]}, {question: 'How do you round the number 7.25, to the nearest integer?', answer: [{text: 'rnd(7.25)', value:false}, {text:'Math.rnd(7.25)', value: false}, {text: 'round(7.25)', value: false}, {text: 'Math.round(7.25)', value: true}]}, {question: 'How do you find the number with the highest value of x and y?', answer: [{text: 'ceil(x, y)', value:false}, {text:'top(x, y)', value: false}, {text: 'Math.ceil(x, y)', value: false}, {text: 'Math.max(x, y)', value: true}]}, {question: 'What is the correct JavaScript syntax to change the content of the HTML element "<p id="demo">This is a demonstration.</p>"?', answer: [{text: 'document.getElementByName("p").innerHTML = "Hello World!";', value:false}, {text:'#demo.innerHTML = "Hello World!";', value: false}, {text: 'document.getElement("p").innerHTML = "Hello World!";', value: false}, {text: 'document.getElementById("demo").innerHTML = "Hello World!";', value: true}]}];
+  {question: 'Inside which HTML element do we put the JavaScript?', answer: [{text: '<script>', value: true}, {text: '<scripting>', value:false}, {text:'<javascript>', value: false}, {text: '<js>', value: false}]}, {question: 'How do you write "Hello World" in an alert box?', answer: [{text: 'msgBox("Hello World")', value:false}, {text:'msg("Hello World")', value: false}, {text: 'alertBox("Hello World")', value: false}, {text: 'alert("Hello World")', value: true}]}, {question: 'How do you create a function in JavaScript?', answer: [{text: 'myFunction() = function', value:false}, {text:'function myFunction()', value: true}, {text: 'function:myFunction()', value: false}, {text: 'function = myFunction()', value: false}]}, {question: 'How do you call a function named "myFunction"?', answer: [{text: 'myFunction', value:false}, {text:'call function myFunction()', value: false}, {text: 'call myFunction()', value: false}, {text: 'myFunction()', value: true}]}, {question: 'How to write an IF statement in JavaScript?', answer: [{text: 'if i == 5 then', value:false}, {text:'if i = 5', value: false}, {text: 'if i = 5 then', value: false}, {text: 'if (i == 5)', value: true}]}, {question: 'How does a FOR loop start?', answer: [{text: 'for (i = 0; i <= 5)', value:false}, {text:'for (i <= 5; i++)', value: false}, {text: 'for i = 1 to 5', value: false}, {text: 'for (i = 0; i <= 5; i++)', value: true}]}, {question: 'What is the correct way to write a JavaScript array?', answer: [{text: 'var colors = "red", "green", "blue"', value:false}, {text:'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', value: false}, {text: 'var colors = (1:"red", 2:"green", 3:"blue")', value: false}, {text: 'var colors = ["red", "green", "blue"]', value: true}]}, {question: 'How do you round the number 7.25, to the nearest integer?', answer: [{text: 'rnd(7.25)', value:false}, {text:'Math.rnd(7.25)', value: false}, {text: 'round(7.25)', value: false}, {text: 'Math.round(7.25)', value: true}]}, {question: 'How do you find the number with the highest value of x and y?', answer: [{text: 'ceil(x, y)', value:false}, {text:'top(x, y)', value: false}, {text: 'Math.ceil(x, y)', value: false}, {text: 'Math.max(x, y)', value: true}]}, {question: 'What is the correct JavaScript syntax to change the content of the HTML element "<p id="demo">This is a demonstration.</p>"?', answer: [{text: 'document.getElementByName("p").innerHTML = "Hello World!";', value:false}, {text:'#demo.innerHTML = "Hello World!";', value: false}, {text: 'document.getElement("p").innerHTML = "Hello World!";', value: false}, {text: 'document.getElementById("demo").innerHTML = "Hello World!";', value: true}]}];
 var start = document.getElementById('start');
 var questions = document.getElementById('questions');
-var timeRemaining = 60;
+var timeRemaining = 80;
 var highTime = document.getElementById('high-time');
 var hiddenTimeTracker = document.createElement('div');
+var feedback = document.getElementById('showfeedback');
+var answer;
+var correctOrWrong;
 hiddenTimeTracker.id = 'hidden';
 hiddenTimeTracker.className = 'hidden';
 highTime.appendChild(hiddenTimeTracker);
 var quiz = function() {
   var i = 0;
+  function buildFeedback() {
+    feedback.textContent = null;
+    console.log(answer);
+    i++;
+    if (answer) {
+      correctOrWrong = "Correct!";
+    } else {
+      correctOrWrong = "Wrong!";
+    };
+    var showFeedback = document.createElement('span');
+    showFeedback.textContent = correctOrWrong;
+    feedback.appendChild(showFeedback);
+    questionBuilder();
+  };
   function questionBuilder() {
     if (i < dataBase.length) {
-      var newDivEl = document.createElement('div');
+      newDivEl = document.createElement('div');
       newDivEl.id = ('question');
       newDivEl.className = ('questions');
       questions.appendChild(newDivEl);
@@ -27,16 +44,14 @@ var quiz = function() {
       var value1 = (dataBase[i].answer[0].value);
       answer1El.addEventListener('click', function() {
         if (value1) {
-          console.log("TRUE");
-          i++;
+          answer = true;
           newDivEl.style.display = "none";
-          questionBuilder();
+          buildFeedback();
         } else if (!value1) {
-          console.log("FALSE");
-          i++;
+          answer = false;
           newDivEl.style.display = "none";
           timeRemaining = (timeRemaining - 10);
-          questionBuilder();
+          buildFeedback();
         }
       });
       var answer2El = document.createElement('button');
@@ -47,16 +62,14 @@ var quiz = function() {
       var value2 = (dataBase[i].answer[1].value);
       answer2El.addEventListener('click', function() {
         if (value2) {
-          console.log("TRUE");
-          i++;
+          answer = true;
           newDivEl.style.display = "none";
-          questionBuilder();
+          buildFeedback();
         } else if (!value2) {
-          console.log("FALSE");
-          i++;
+          answer = false;
           newDivEl.style.display = "none";
           timeRemaining = (timeRemaining - 10);
-          questionBuilder();
+          buildFeedback();
         }
       });
       var answer3El = document.createElement('button');
@@ -67,16 +80,14 @@ var quiz = function() {
       var value3 = (dataBase[i].answer[2].value);
       answer3El.addEventListener('click', function() {
         if (value3) {
-          console.log("TRUE");
-          i++;
+          answer = true;
           newDivEl.style.display = "none";
-          questionBuilder();
+          buildFeedback();
         } else if (!value3) {
-          console.log("FALSE");
-          i++;
+          answer = false;
           newDivEl.style.display = "none";
           timeRemaining = (timeRemaining - 10);
-          questionBuilder();
+          buildFeedback();
         }
       });
       var answer4El = document.createElement('button');
@@ -87,16 +98,14 @@ var quiz = function() {
       var value4 = (dataBase[i].answer[3].value);
       answer4El.addEventListener('click', function() {
         if (value4) {
-          console.log("TRUE");
-          i++;
+          answer = true;
           newDivEl.style.display = "none";
-          questionBuilder();
+          buildFeedback();
         } else if (!value4) {
-          console.log("FALSE");
-          i++;
+          answer = false;
           newDivEl.style.display = "none";
           timeRemaining = (timeRemaining - 10);
-          questionBuilder();
+          buildFeedback();
         }
       });
       return timeRemaining;
@@ -126,6 +135,7 @@ function stopTimer() {
   var gameOver = document.querySelector('#questions');
   gameOver.className = 'gameOver';
   gameOver.innerHTML = '<span>Time is up!</span>';
+  feedback.textContent = null;
 };
 function startTimer() {
   const timeDisplay = document.getElementById('time');
